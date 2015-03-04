14e848cce59ae7618103ed52bdd1d9b6e0c8aa7d
monzurulehsan <monzurul.ehsan@me.com>
2015-03-04 09:45:27 -0500

    09:45

--- test3/ViewController.swift
+++ test3/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.redColor()
+        self.view.backgroundColor = UIColor.greenColor()
     }
 
     override func didReceiveMemoryWarning() {
